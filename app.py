a57e6fbb7d3c272819d527eb334d1904b96f57a7
anandkraws <anandkraws@gmail.com>
2024-12-27 23:33:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, request
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Please run on EKS as well'
+    # Get the IP address of the incoming request
+    pod_ip = request.remote_addr
+    return f'Please run on EKS as well. Connected through pod IP: {pod_ip}'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
